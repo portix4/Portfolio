986e6abbff04b851ddc4e01252f313f53415d1fe
Pablo Portillo <paabloportillo@gmail.com>
2024-01-03 13:48:12 +0100

    projects

--- src/pages/ProjectsPage/Projects.jsx
+++ src/pages/ProjectsPage/Projects.jsx
@@ -1,13 +1,43 @@
 import { Container } from 'react-bootstrap'
-import './Project.css'
+import './Projects.css'
+import { useState } from 'react'
+import foto from './../../assets/profile-pic.jpeg'
 
 const Projects = () => {
+  const [isFlipped, setFlipped] = useState(false)
+
+  const handleMouseEnter = () => {
+    setFlipped(true)
+  }
+
+  const handleMouseLeave = () => {
+    setFlipped(false)
+  }
+
   return (
     <div className='Projects' id='projects'>
       <Container>
         <div className='firstContainer'>
           <h2 className='pt-4'>Projects</h2>
           <div className='underline'></div>
+        </div>
+        <div
+          className={`flip-card ${isFlipped ? 'flipped' : ''}`}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          <div className='flip-card-inner'>
+            <div className='flip-card-front'>
+              {/* Contenido de la cara frontal, como la imagen */}
+              <img src={foto} alt='Imagen' />
+            </div>
+            <div className='flip-card-back'>
+              {/* Contenido de la cara trasera, como el texto */}
+              <div style={{ backgroundColor: 'red' }}>
+                <p>Texto en la cara trasera</p>
+              </div>
+            </div>
+          </div>
         </div>
       </Container>
     </div>
